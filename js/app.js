3b020fae901b5378381d861a44d8c23dc780ba37
Erwin Rosales <look4erwin@gmail.com>
2018-07-06 03:24:15 -0700

    Infowindow appears when a list item is clicked
    Added a click function to the list of place names in the view. When a name is clicked, an infowindow will appear with the appropriate associated marker.
    
    Infowindows that appear contain venue information utilizing the Foursquare API.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-var map, marker;
+var map;
 var markers = [];
-var placeList = [];
+var infowindow;
+var infowindowData;
 
 function initMap() {
   // load map
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 12.75,
     center: {lat: 37.548271, lng:-121.988571},
     disableDefaultUI: true
   });
@@ -16,76 +17,76 @@ function neighborhoodAppViewModel() {
   "use strict";
 
   var self = this;
-  var infowindow = new google.maps.InfoWindow();
+  var marker;
+  var Place = (function(name, lat, lng, id) {
+    this.name = name,
+    this.lat = lat,
+    this.lng = lng,
+    this.id = id;
+  });
+  var placeList = [];
+  var pushMarkersObservableArr;
+  infowindow = new google.maps.InfoWindow();
 
   self.data = [
     {
       "name": "Devout Coffee",
       "lat": 37.577225,
       "lng": -121.980728,
-      "id": "ChIJBUN0jW3Aj4ARiUk9mQkTgQ8"
+      "id": "529a682e11d251a74918e082"
     },
     {
       "name": "Slapface Coffee",
       "lat": 37.5584073,
       "lng": -122.0068662,
-      "id": "ChIJk_RMgJy_j4ARU6hT5aaVlJM"
+      "id": "56ccd05a498eb21aed240776"
     },
     {
       "name": "Mission Coffee",
       "lat": 37.53353329999999,
       "lng": -121.9208885,
-      "id": "ChIJWWCfGrfGj4AR6Hlr8U5ePfE"
+      "id": "4a66da25f964a520d7c81fe3"
     },
     {
       "name": "Suju's Coffee and Tea",
       "lat": 37.529927,
       "lng": -121.9830347,
-      "id": "ChIJIRdYs6_Aj4ARQhW4gPZB_lI"
+      "id": "4b689012f964a52019802be3"
     },
     {
       "name": "Bean Scene Cafe",
       "lat": 37.5327193,
       "lng": -121.9593492,
-      "id": "ChIJJQ32j83Aj4ARvHLRh6S4xhc"
+      "id": "4f4c45d9e4b0c132f4f15726"
     }
   ]
-  self.Place = (function(name, lat, lng, id) {
-    this.name = name,
-    this.lat = lat,
-    this.lng = lng,
-    this.id = id;
-  });
   self.placeList = (function() {
     for(var i = 0; i < self.data.length; i++) {
-      var places = new self.Place(self.data[i].name, self.data[i].lat, self.data[i].lng, self.data[i].id);
+      var places = new Place(self.data[i].name, self.data[i].lat, self.data[i].lng, self.data[i].id);
       placeList.push(places);
     }
   }());
-  self.marker = (function() {
-    var lat, lng, pos;
-    // var bounds = new google.maps.LatLngBounds();
+  marker = (function() {
+    var lat, lng;
     for(var j = 0; j < placeList.length; j++) {
-      lat = placeList[j].lat;
-      lng = placeList[j].lng;
-      pos = {lat, lng};
       marker = new google.maps.Marker({
-        position: pos,
+        position: {
+          lat: placeList[j].lat,
+          lng: placeList[j].lng},
         title: placeList[j].name,
         id: placeList[j].id
       })
       marker.setMap(map);
       // Push marker into array of markers
       markers.push(marker);
-      // bounds.extend(marker.position);
       marker.addListener('click', function() {
-        self.infowindowData(this, infowindow);
+        infowindow.setContent(null);
+        self.infowindowData(this, infowindow, this.title);
       })
     }
-    // map.fitBounds(bounds);
   }());
   self.markersObservableArr = ko.observableArray();
-  self.pushMarkersObservableArr = (function() {
+  pushMarkersObservableArr = (function() {
     for(var i = 0; i < self.data.length; i++) {
       self.markersObservableArr.push(self.data[i]);
     }
@@ -110,27 +111,47 @@ function neighborhoodAppViewModel() {
     // Filter markers depending on list of place names
     return true;
     }
-  self.infowindowData = function (marker, infowindow) {
+  self.infowindowData = function (marker, infowindow, name) {
     // Ajax request to Foursquare
-    var foursquare = $.ajax({
+    var xhr = $.ajax({
       method: 'GET',
-      url: "https://api.foursquare.com/v2/venues/search?",
+      url: "https://api.foursquare.com/v2/venues/search?near=Fremont,CA",
       dataType: 'json',
-      data: 'client_id=NEUORKAG245XLYEPTJRLI31AE4UJH0BRUKA3FZJFJOPVHGMW' +
+      data: '&client_id=NEUORKAG245XLYEPTJRLI31AE4UJH0BRUKA3FZJFJOPVHGMW' +
             '&client_secret=Q5AUJAPD2OOZO3EXLWPOTQPZVUX4ML3G15JQYLHSQUO24K15' +
-            '&near=Fremont,CA' +
+            '&query=' + name +
             '&v=20180609' +
-            '&limit=1'
+            '&limit=1',
+      async: true,
+      // When AJAX request is succesful, populate infowindow with associated
+      // place information provided by Foursquare API
+      success: function() {
+        var info = JSON.parse(xhr.responseText).response.venues[0];
+        var infoName = info.name;
+        var infoCategory = info.categories[0].name;
+        var infoAddress = info.location.formattedAddress[0] + '<br>' + info.location.formattedAddress[1] + '<br>' + info.location.formattedAddress[2];
+        infowindow.setContent('<h3>' + infoName + '</h3> <div>' + infoAddress + '</div><p><i>--' + infoCategory + '--</i></p>');
+        console.log(info);
+      }
     })
     // Check to see if associated marker has infowindow open
     if(infowindow.marker != marker) {
       infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
       infowindow.open(map, marker);
       // Clear property marker
       infowindow.addListener('closeclick', function() {
         infowindow.setMarker(null);
       })
+    }
+  }
+  self.listItemClick = function() {
+    // When a list item is clicked in the view,
+    // it's associated marker will populate an attached infowindow
+    var listItem = this.name;
+    for(var i = 0; i < markers.length; i++) {
+      if(listItem == markers[i].title) {
+        self.infowindowData(markers[i], infowindow, markers[i].title);
+      }
     }
   }
 }
